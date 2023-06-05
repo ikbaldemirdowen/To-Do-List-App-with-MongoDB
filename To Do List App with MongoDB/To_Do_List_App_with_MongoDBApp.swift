dde67d8d0d9ed54676c1085ebc5ba930ce539e99
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-06-04 19:43:44 -0700

    Initial Commit

---
+++ To Do List App with MongoDB/To_Do_List_App_with_MongoDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_List_App_with_MongoDBApp.swift
+//  To Do List App with MongoDB
+//
+//  Created by Ikbal Demirdoven on 2023-06-04.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_List_App_with_MongoDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
